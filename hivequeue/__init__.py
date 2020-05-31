d1c1ee8e86c9414d869896acdba34666b2e365ab
Rob J Meijer <rob@dnpa.nl>
2020-06-01 01:19:12 +0200

    merged different rate limit implementations into one

---
+++ hivequeue/__init__.py
@@ -0,0 +1,2 @@
+from .rate import RateLimit
+
--- hivequeue/rate.py
+++ hivequeue/rate.py
@@ -9,106 +9,89 @@ def retry_to_seconds(retry):
     except:
         return None
 
-
-class LimitNone:
-    def __init__(self, funct, loop):
-        self.funct = funct
-        self.loop = loop
-
-    def __call__(self, *args, **kwargs):
-        self.loop.call_soon(self.funct(*args, **kwargs))
-
-    def headers(self, headers):
-        pass
-
-
-class LimitWindow:
-    def __init__(self, funct, loop, limit, interval=60):
-        self.funct = funct
-        self.loop = loop
-        self.limit = limit
-        self.interval = interval
-        self.interval_start = 0
-        self.interval_count = 0
-
-    def __call__(self, *args, **kwargs):
-        loop_on = True
-        while loop_on:
-            now = int(time.time())
-            if self.interval_start + self.interval < now:
-                self.interval_start = now
-                self.interval_count = 0
-            if self.interval_count < self.limit:
-                self.interval_count += 1
-                self.loop.call_soon(self.funct(*args, **kwargs))
-                loop_on = False
-            else:
-                time_left = self.interval + self.interval_start - time.time()
-                if time_left > 0.0:
-                    asyncio.sleep(time_left)
-                else:
-                    self.interval_count += 1
-                    self.loop.call_soon(self.funct(*args, **kwargs))
-                    loop_on = False
-
-    def headers(self, headers):
-        pass
-
-
-class LimitCredits:
-    def __init__(self, funct, loop, rate, startboost=0, ceiling=100):
-        self.funct = funct
-        self.loop = loop
-        self.rate = rate
-        self.credits = startboost
-        self.ceiling = ceiling
-        self.last_time = time.time()
-
-    def __call__(self, *args, **kwargs):
-        loop_on = True
-        while loop_on:
-            now = time.time()
-            self.credits = (now - self.last_time) * self.rate
-            if self.credits > self.ceiling:
-                self.credits = self.ceiling
-            self.last_time = now
-            if credits >= 1.0:
-                self.credits -= 1.0
-                self.loop.call_soon(self.funct(*args, **kwargs))
-                loop_on = False
-            else:
-                credits_needed = 1.0001 - self.credits
-                time_to_credit = credits_needed / self.rate
-                asyncio.sleep(time_to_credit)
-
-    def headers(self, headers):
-        pass
+class FakeServer:
+    def __init__(self, fallback_window, fallback_count):
+        self.fallback_window = fallback_window
+        self.fallback_count = fallback_count
+        if self.fallback_count is None:
+            self.fallback_window = None
+        if self.fallback_window is not None:    
+            self.curwindow_end = int(time.time()) + self.fallback_window
+            self.curwindow_count = 0
+    def __call__(self):
+        if self.fallback_window is None:
+            return {
+                "RateLimit-Limit": "1000",
+                "RateLimit-Remaining": "999",
+                "RateLimit-Reset": "1"
+            }
+        now = int(time.time())
+        if now < self.curwindow_end:
+            self.curwindow_count += 1
+        else:
+            self.curwindow_count = 1
+            while time.time() >= self.curwindow_end:
+                self.curwindow_end += self.fallback_window
+        rval = dict()
+        rval["RateLimit-Limit"] = str(self.fallback_count) + \
+                                  ", " + str(self.fallback_count) + \
+                                  ";window=" + str(self.fallback_window)
+        remaining = self.fallback_count - self.curwindow_count
+        if remaining < 0:
+            rval["RateLimit-Remaining"] = "0"
+            rval["Retry-After"] = str(self.curwindow_end - now)
+        else:
+            rval["RateLimit-Remaining"] = str(remaining)
+        rval["RateLimit-Remaining"] = str(self.fallback_count - self.curwindow_count)
+        rval["RateLimit-Reset"] = str(self.curwindow_end - now)
+        return rval
 
 
 # draft-polli-ratelimit-headers-00
-class LimitPolli:
-    def __init__(self, funct, loop):
+class RateLimit:
+    def __init__(self,
+                 funct,                 # The function to rate limit
+                 loop,                  # The asyncio loop
+                 polli_spare=0,         # We may not want to completely exaust the remaining units
+                 fallback_window=None,  # If no Polli headers are given on probe, generate fake
+                                        #  headers from provided fallback pollicy.
+                 fallback_count=None
+                ):
         self.funct = funct
         self.loop = loop
-        self.has_headers = False
+        self.polli_spare = polli_spare
+        self.fakeserver = FakeServer(fallback_window, fallback_count)
+        self.probed = False
+        self.use_fallback = False
         self.behind = 0
+        self.policies = []
         self.limit = None
         self.remaining = None
         self.reset = None
+        self.retry = False
 
     def __call__(self, *args, **kwargs):
+        # Loop untill call soon is called and loop_on set to False
         loop_on = True
+        # New call not yet returned, means we will be one extra behind when invoking
         self.behind += 1
         while loop_on:
-            if not self.has_headers:
+            #If this is the first call, call directly, need a response with headers before
+            # rate limit can commence.
+            if not self.probed:
                 self.loop.call_soon(self.funct(*args, **kwargs))
                 loop_on = False
             else:
-                if self.remaining and self.remaining > 0:
+                if self.use_fallback:
+                    # Use fallback if needed
+                    self.headers(200, self.fakeserver(), fallback=True)
+                # If within the rules of the rate limiter, call soon.
+                if self.remaining and self.remaining > self.polli_spare:
                     self.remaining -= 1
                     self.loop.call_soon(self.funct(*args, **kwargs))
                     loop_on = False
                 else:
+                    #If not, wait for the reset moment.
                     waitfor = self.reset - time.time()
                     if waitfor <= 0.0:
                         self.loop.call_soon(self.funct(*args, **kwargs))
@@ -116,40 +99,38 @@ class LimitPolli:
                     else:
                         asyncio.sleep(waitfor)
 
-    def headers(self, headers):
-        self.behind -= 1
-        if "RateLimit-Limit" in headers:
-            self.limit = int(headers["RateLimit-Limit"].split(",")[0])
-        else:
-            self.limit = None
-        if "RateLimit-Remaining" in headers:
-            self.remaining = int(headers["RateLimit-Remaining"]) - self.behind
-        else:
-            self.limit = None
-        if "RateLimit-Reset" in headers:
-            self.reset = headers["RateLimit-Reset"]
-            if not self.reset.isdigit():
-                self.reset = retry_to_seconds(self.reset)
+    def headers(self, status, headers, fallback=False):
+        if not fallback:
+            # One less behind if these are real headers
+            self.behind -= 1
+        # Process RateLimit headers depending on mode
+        if fallback or not self.use_fallback or not self.probed:
+            if "RateLimit-Limit" in headers:
+                self.limit = int(headers["RateLimit-Limit"].split(",")[0])
             else:
-                self.reset += time.time()
-        elif "Retry-After" in headers:
+                self.limit = None
+            if "RateLimit-Remaining" in headers:
+                self.remaining = int(headers["RateLimit-Remaining"]) - self.behind
+            else:
+                self.limit = None
+            if not fallback or not self.retry:
+                if "RateLimit-Reset" in headers:
+                    self.reset = headers["RateLimit-Reset"]
+                    if not self.reset.isdigit():
+                        self.reset = retry_to_seconds(self.reset)
+                    else:
+                        self.reset += time.time()
+                else:
+                    self.reset = None
+            if not self.probed:
+                self.probed = True
+                if self.limit is not None or self.remaining is not None or self.reset is not None:
+                    self.use_fallback = False
+                else:
+                    self.use_fallback = True
+        if "Retry-After" in headers:
             self.reset = retry_to_seconds(self.reset)
+            self.remaining = 0
+            self.retry = True
         else:
-            self.reset = None
-        if not self.has_headers:
-            if self.limit is not None:
-                self.has_headers = True
-            if self.remaining is not None:
-                self.has_headers = True
-            if self.reset is not None:
-                self.has_headers = True
-        else:
-            something = False
-            if self.limit is not None:
-                something = True
-            if self.remaining is not None:
-                something = True
-            if self.reset is not None:
-                something = True
-            if not something:
-                self.has_headers = False
+            self.retry = False
