5445b2076c5b1e1070f0bd653bb1a9b629c5d8c5
Rob J Meijer <rob@dnpa.nl>
2020-06-01 02:00:28 +0200

    some naming

--- hivequeue/__init__.py
+++ hivequeue/__init__.py
@@ -1,2 +1,11 @@
-from .rate import RateLimit
+from .rate import RateLimit # Support for draft-polli-ratelimit-headers-00 and simple static windows
+# from .rate import BackOff # Backoff policy on API server failure.
+# from .client import RpcClient # The client and queue consumer, 
+                                # one client per API node queue combination
+# from .queue import RpcQueue   # The priority/hysteresis queue, one queue handled by one or 
+                                # more clients.
+# from .blocks import LifeStream # Stream new blocks
+# from .blocks import LagStream  # Stream old blocks with a given lag (for example 7 days)
+# from .crypto import SigningQueue # A signing key bound high priority queue for signed operations.
+                                   #  SigningQueue chains to RpcQueue
 
